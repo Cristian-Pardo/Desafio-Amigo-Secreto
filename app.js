7671d652637a1cc9cb366fd38c208b5eadf6ef86
Cristian-Pardo <ccpardo4@gmail.com>
2025-02-22 15:13:29 -0500

    funcionalidad estable para listar los amigos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 //Variables. 
-let listaAmigos=[];
+let variableListaAmigos=[];
 
 function agregarAmigo() {
     
@@ -14,16 +14,16 @@ function agregarAmigo() {
     returnValidadorCampo=validadorInputNoVacio(valorIngresadoUsuario);
     
     if(returnValidadorCampo){
-        listaAmigos.push(valorIngresadoUsuario)
+        variableListaAmigos.push(valorIngresadoUsuario)
         valorIngresadoUsuario=""
     }
     else {
-        console.log("   No se cumplieron politicas establecidas por lo tanto no se actualiza listaAmigos ");
+        console.log("   No se cumplieron politicas establecidas por lo tanto no se actualiza variableListaAmigos ");
         
         alert("Por favor, inserte un nombre.");
     }
 
-    console.log(listaAmigos);
+    console.log(variableListaAmigos);
 }
 
 //esta funcion valida si esta vacio y devuelve un buleano. 
@@ -37,7 +37,58 @@ function validadorInputNoVacio(datoIngresado) {
     if (datoIngresado.trim()!=""){
         console.log("   ingreso al if porque el datoIngresado es diferente a vacio");
         datoConInfo=true;
-        console.log("   lo que llego al if: " + datoIngresado);
+        console.log("   valor evaluado en el if: " + datoIngresado);
     }
     return datoConInfo;
+}
+
+
+
+//////// hasta aqui bien 
+
+function recorrerArrayAmigos(variableListaAmigos) {
+    console.log("Ingreso a la funcion recorrerArrayAmigos");
+    
+    //variable listaAmigosJS ahora tiene la referencia al <ul> del id empleado en el index
+    let listaAmigosJS=document.getElementById("listaAmigos");
+    
+    variableListaAmigos.forEach(nombreAmigo => {
+        const li = document.createElement("li"); // Creamos un elemento <li>
+        li.textContent = nombreAmigo; // Asignamos el texto del amigo al <li>
+        listaAmigosJS.appendChild(li); // Agregamos el <li> dentro del <ul>
+        console.log("En este apartado ingresa cada amigo a ul "+li);
+    });
+
+
+
+    console.log(variableListaAmigos);
+    
+
+console.log("Estado actual del resultado: "+resultado);
+
+}
+
+
+
+
+
+
+
+//Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. 
+// Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+
+//Tareas específicas:
+//Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+
+//Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+
+//Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+
+//Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+
+
+
+
+function sortearAmigo(){
+    console.log("Ingreso a la funcion sortearAmigo");
 }
