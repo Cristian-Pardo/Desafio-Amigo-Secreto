65d434de1de0f426f12dc20be4881c372d903d9d
Cristian-Pardo <ccpardo4@gmail.com>
2025-02-22 04:34:19 -0500

    Actualiza el .js Agregando funcion amigos

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variables. 
+let listaAmigos=[]
+console.log(listaAmigos)
+
+function agregarAmigos(params) {
+    
+}
