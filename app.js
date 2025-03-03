79d064e40eab68c9279e84a98d4efa6cf2f33cbd
Cristian-Pardo <ccpardo4@gmail.com>
2025-03-03 10:54:36 -0500

    Correccion funcion recorrerArrayAmigos

--- app.js
+++ app.js
@@ -1,75 +1,79 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-//Variables. 
+//Array. 
 let variableListaAmigos=[];
 
-function agregarAmigo() {
-    
-    console.log("Ingreso a la funcion agregarAmigo");
-    let returnValidadorCampo=false;
-    let valorIngresadoUsuario="";
+//comienzo para agregar amigo.
 
-    valorIngresadoUsuario=(document.getElementById('amigo').value);
-    console.log("   Dato ingresado por el usuario: "+valorIngresadoUsuario);
-    returnValidadorCampo=validadorInputNoVacio(valorIngresadoUsuario);
-    
-    if(returnValidadorCampo){
-        variableListaAmigos.push(valorIngresadoUsuario)
-        valorIngresadoUsuario=""
-    }
-    else {
-        console.log("   No se cumplieron politicas establecidas por lo tanto no se actualiza variableListaAmigos ");
+    function agregarAmigo() {
         
-        alert("Por favor, inserte un nombre.");
+        console.log("Ingreso a la funcion agregarAmigo");
+        let returnValidadorCampo=false;
+        let valorIngresadoUsuario="";
+
+        valorIngresadoUsuario=(document.getElementById('amigo').value);
+        console.log("   Dato ingresado por el usuario: "+valorIngresadoUsuario);
+        returnValidadorCampo=validadorInputNoVacio(valorIngresadoUsuario);
+        
+        if(returnValidadorCampo){
+            variableListaAmigos.push(valorIngresadoUsuario)
+            valorIngresadoUsuario=""
+        }
+        else {
+            console.log("   No se cumplieron politicas establecidas por lo tanto no se actualiza variableListaAmigos ");
+            
+            alert("Por favor, inserte un nombre.");
+        }
+        limpiarCampo=(document.getElementById("amigo"));
+        limpiarCampo.value="";
+        // la linea . focus te permite hacer que el cursor de texto aparezca de nuevo en el recuadro del input
+        // evita volver a dar clic.
+        limpiarCampo.focus();
+        console.log(variableListaAmigos);
+        recorrerArrayAmigos();
     }
-    limpiarCampo=(document.getElementById("amigo"));
-    limpiarCampo.value="";
-    console.log(variableListaAmigos);
-}
 
-//esta funcion valida si esta vacio y devuelve un buleano. 
-function validadorInputNoVacio(datoIngresado) {
-    console.log("Ingreso a la funcion validadorInputVacio");
-    
-    let datoConInfo=false
-    console.log("   El tamaño de texto ingresado sin espacios es " + datoIngresado.trim().length);
-    
+    //esta funcion valida si esta vacio y devuelve un buleano. 
+    function validadorInputNoVacio(datoIngresado) {
+        console.log("Ingreso a la funcion validadorInputVacio");
+        
+        let datoConInfo=false
+        console.log("   El tamaño de texto ingresado sin espacios es " + datoIngresado.trim().length);
+        
 
-    if (datoIngresado.trim()!=""){
-        console.log("   ingreso al if porque el datoIngresado es diferente a vacio");
-        datoConInfo=true;
-        console.log("   valor evaluado en el if: " + datoIngresado);
+        if (datoIngresado.trim()!=""){
+            console.log("   ingreso al if porque el datoIngresado es diferente a vacio");
+            datoConInfo=true;
+            console.log("   valor evaluado en el if: " + datoIngresado);
+        }
+        return datoConInfo;
     }
-    return datoConInfo;
-}
+
+//Fin Agregar Amigo.
 
 
+//Comienzo mostrar Amigos
 
-//////// hasta aqui bien 
+    function recorrerArrayAmigos() {
+        console.log("Ingreso a la funcion recorrerArrayAmigos");
+        
+        let listaAmigosJS=document.getElementById("listaAmigos");
+        //variable listaAmigosJS ahora tiene la referencia al <ul> del id empleado en el index
+        listaAmigosJS.innerHTML=""
 
-function recorrerArrayAmigos(variableListaAmigos) {
-    console.log("Ingreso a la funcion recorrerArrayAmigos");
+
+        for (let index = 0; index < variableListaAmigos.length; index++) {
+            let li = document.createElement("li"); // Creamos un elemento <li>
+            li.textContent = variableListaAmigos[index]; // Asignamos el texto del amigo al <li>
+            console.log("En este apartado ingresa cada amigo a ul valor Agregado"+ variableListaAmigos[index]);
+            listaAmigosJS.appendChild(li); // Agregamos el <li> dentro del <ul> usamos la variable JS para ello (Asi nos evitamos invocar nuevamente el getElem...)
+            console.log(variableListaAmigos);
+        }
+        
+        console.log("Estado actual del resultado: Exitoso");
+    }
     
-    //variable listaAmigosJS ahora tiene la referencia al <ul> del id empleado en el index
-    let listaAmigosJS=document.getElementById("listaAmigos");
-    
-    variableListaAmigos.forEach(nombreAmigo => {
-        const li = document.createElement("li"); // Creamos un elemento <li>
-        li.textContent = nombreAmigo; // Asignamos el texto del amigo al <li>
-        listaAmigosJS.appendChild(li); // Agregamos el <li> dentro del <ul>
-        console.log("En este apartado ingresa cada amigo a ul "+li);
-    });
-
-
-
-    console.log(variableListaAmigos);
-    
-
-console.log("Estado actual del resultado: "+resultado);
-
-}
-
-
+//Fin mostrar Amigos
 
 
 
